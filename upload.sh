6bd8fed75498a9ce32e34399dfdde2cb950cb67a
Kirthi Raman <kirthi.raman@gmail.com>
2019-04-05 12:40:29 -0400

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Upload Started"
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+do
+  aws s3 cp /mnt/SecureCluster/user/mobiletl/scanbuy_results/Scanbuy_AdAdvisor_results_20190401_$i.csv.gz s3://neustar-scanbuy/
+done
